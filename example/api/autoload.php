058fcaee6b06356966f4ab711797847f884652c4
Simon Hatt <hatt@google.com>
2021-10-31 18:47:51 +0000

    Write README (#11)

--- example/api/autoload.php
+++ example/api/autoload.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Do not use this file in your project!
+ * Instead, require php-typescript-api in composer:
+ *
+ *     composer require allestuetsmerweh/php-typescript-api
+ *
+ */
 include_once __DIR__.'/../../vendor/autoload.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
